f432784a01cfe77884ca4962bd1b2cfa66d7c78f
min103jun <min103jun@naver.com>
2019-06-15 19:29:30 +0900

    Update turtle_recognize_color.cpp

--- turtle_recognize_color.cpp
+++ turtle_recognize_color.cpp
@@ -20,20 +20,14 @@ using namespace cv;
 using namespace std;
 ros::Publisher pub;
 
-void poseMessageReceivedRGB(const sensor_msgs::ImageConstPtr &msg)
+
+int detectColor(Scalar want2detect, Mat img_frame)
 {
-    Mat img_frame = cv_bridge::toCvShare(msg, "bgr8")->image;
     Mat img_hsv;
     
     int range_count = 0;
 
-	Scalar red(0, 0, 255);
-	Scalar blue(255, 0, 0);
-	Scalar yellow(0, 255, 255);
-	Scalar magenta(255, 0, 255);
-
-
-	Mat rgb_color = Mat(1, 1, CV_8UC3, red);
+	Mat rgb_color = Mat(1, 1, CV_8UC3, want2detect); // <<-- 여기 색상을 바꿔줘야 함.
 	Mat hsv_color;
 
 	cvtColor(rgb_color, hsv_color, COLOR_BGR2HSV);
@@ -41,11 +35,6 @@ void poseMessageReceivedRGB(const sensor_msgs::ImageConstPtr &msg)
     int hue = (int)hsv_color.at<Vec3b>(0, 0)[0];
 	int saturation = (int)hsv_color.at<Vec3b>(0, 0)[1];
 	int value = (int)hsv_color.at<Vec3b>(0, 0)[2];
-
-
-	cout << "hue = " << hue << endl;
-	cout << "saturation = " << saturation << endl;
-	cout << "value = " << value << endl;
 
 	int low_hue = hue - 4;
 	int high_hue = hue + 4;
@@ -56,12 +45,12 @@ void poseMessageReceivedRGB(const sensor_msgs::ImageConstPtr &msg)
 		range_count = 2;
 
 		high_hue1 = 180;
-		low_hue1 = low_hue + 180;
+		low_hue1 = low_hue + 170;
 		high_hue2 = high_hue;
 		low_hue2 = 0;
 	}
 
-	else if (high_hue > 170) {
+	else if (high_hue > 180) {
 		range_count = 2;
 
 		high_hue1 = low_hue;
@@ -77,13 +66,8 @@ void poseMessageReceivedRGB(const sensor_msgs::ImageConstPtr &msg)
 		high_hue1 = high_hue;
 	}
 
-
-	cout << low_hue1 << "  " << high_hue1 << endl;
-	cout << low_hue2 << "  " << high_hue2 << endl;
-
-		//HSV로 변환
+	//HSV로 변환
 	cvtColor(img_frame, img_hsv, COLOR_BGR2HSV);
-
 
 	//지정한 HSV 범위를 이용하여 영상을 이진화
 	Mat img_mask1, img_mask2;
@@ -93,24 +77,20 @@ void poseMessageReceivedRGB(const sensor_msgs::ImageConstPtr &msg)
 		img_mask1 |= img_mask2;
 	}
 
-
 	//morphological opening 작은 점들을 제거 
 	erode(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 	dilate(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
-
 	//morphological closing 영역의 구멍 메우기 
 	dilate(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 	erode(img_mask1, img_mask1, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
 
 	//라벨링 
 	Mat img_labels, stats, centroids;
 	int numOfLables = connectedComponentsWithStats(img_mask1, img_labels,
 			stats, centroids, 8, CV_32S);
 
-
-		//영역박스 그리기
+	//영역박스 그리기
 	int max = -1, idx = 0;
 	for (int j = 1; j < numOfLables; j++) {
 		int area = stats.at<int>(j, CC_STAT_AREA);
@@ -120,35 +100,70 @@ void poseMessageReceivedRGB(const sensor_msgs::ImageConstPtr &msg)
 			idx = j;
 		}
 	}
-		
 
 	int left = stats.at<int>(idx, CC_STAT_LEFT);
 	int top = stats.at<int>(idx, CC_STAT_TOP);
 	int width = stats.at<int>(idx, CC_STAT_WIDTH);
 	int height = stats.at<int>(idx, CC_STAT_HEIGHT);
-    if(left && top)
-        if(width && height)
-        {
-            cout<<"<<red detected>>"<<endl;
-                geometry_msgs::Twist baseCmd;
-                baseCmd.linear.x=0;
-                baseCmd.linear.y=0;
-                baseCmd.linear.z=0;
 
-                baseCmd.angular.x = 0;
-                baseCmd.angular.y = 0;
-                baseCmd.angular.z = 0;
-           	pub.publish(baseCmd);
-		sleep(1);
-        }
-            
+    if(left && top && width && height) {
+		rectangle(img_frame, Point(left, top), Point(left + width, top + height), Scalar(0, 0, 255), 1);
+		imshow("squares", img_frame);
+		waitKey(30);
+		return 1;
+	}
 
-	rectangle(img_frame, Point(left, top), Point(left + width, top + height),
-			Scalar(0, 0, 255), 1);
-
-    imshow("img_origin", img_mask1);
-    imshow("squares", img_frame);
     waitKey(30);
+	return 0;
+}
+
+void sendMessage(const sensor_msgs::ImageConstPtr &msg){
+
+	Mat img_frame = cv_bridge::toCvShare(msg, "bgr8")->image;
+	Scalar red(0, 0, 255);
+	Scalar blue(255, 0, 0);
+	Scalar yellow(0, 255, 255);
+	Scalar magenta(255, 0, 255);
+
+	int resultRed = detectColor(red, img_frame);
+	int resultBlue = detectColor(blue, img_frame);
+	int resultYellow = detectColor(yellow, img_frame);
+
+	if(resultRed + resultBlue + resultYellow == 1) {
+		if(resultBlue == 1) {
+			cout << "<<blue detected>>" << endl;
+			/*geometry_msgs::Twist baseCmd;
+            baseCmd.linear.x=1.0;
+            baseCmd.linear.y=1.0;
+            baseCmd.linear.z=1.0;
+			pub.publish(baseCmd);*/
+			return;
+		}
+		else if(resultRed == 1) {
+			cout<<"<<red detected>>"<<endl;
+            /*geometry_msgs::Twist baseCmd;
+            baseCmd.linear.x=0;
+            baseCmd.linear.y=0;
+            baseCmd.linear.z=0;
+
+            baseCmd.angular.x = 0;
+            baseCmd.angular.y = 0;
+            baseCmd.angular.z = 0;
+           	pub.publish(baseCmd);
+			sleep(1);*/
+			return;
+		}
+		else if(resultYellow == 1) {
+			cout << "<<yellow detected>>" << endl;
+			return;
+		}
+	}
+	else if(resultRed + resultBlue + resultYellow == 0){
+		cout << "<<not detected>>" << endl;
+	}
+	else {
+		cout << "<<multiple detect>>" << endl;
+	}
 }
 
 int main(int argc, char **argv)
@@ -161,7 +176,7 @@ int main(int argc, char **argv)
 
     image_transport::ImageTransport it(nh);
     // Create a subscriber object
-    image_transport::Subscriber subRGB = it.subscribe("/raspicam_node/image", 1, &poseMessageReceivedRGB, ros::VoidPtr(), image_transport::TransportHints("compressed"));
+    image_transport::Subscriber subRGB = it.subscribe("/raspicam_node/image", 1, &sendMessage, ros::VoidPtr(), image_transport::TransportHints("compressed"));
     // Let ROS take over
     ros::spin();
     return 0;
